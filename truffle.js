81632dcd03ea4558e7294cd272db1cad87cc1622
kendan <kendan@blvnp.com>
2018-11-04 15:41:53 +0800

    all contracts corrected to version 0.4.19

--- truffle.js
+++ truffle.js
@@ -4,14 +4,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
-    },
-    home: {
-      host: "192.168.1.16",
-      port: 1201,
-      gas: 7984452,
-      gasLimit: 7984452,
-      gasPrice: 2000000000,
-      network_id: "*"
     }
   },
   compiler: {
